caa4a2268e8d8ea0bec4a86b353afde3fde3fa14
Narendren S <106636304+NARENDREN2003@users.noreply.github.com>
2025-03-28 21:58:38 +0530

    Add files via upload

---
+++ DAY 3-Digital Logic with TL-Verilog and Makerchip/PIPELINED_LOGIC/PYTHAGORES_THEOREM.v
@@ -0,0 +1,29 @@
+\m5_TLV_version 1d: tl-x.org
+\m5
+   
+   // =================================================
+   // Welcome!  New to Makerchip? Try the "Learn" menu.
+   // =================================================
+   
+   //use(m5-1.0)   /// uncomment to use M5 macro library.
+\SV
+   // Macro providing required top-level module definition, random
+   // stimulus support, and Verilator config.
+   m5_makerchip_module   // (Expanded in Nav-TLV pane.)
+   `include "sqrt32.v"
+\TLV
+   $reset = *reset;
+   |calc
+      @1          //PYTHAGORES THEOREM
+         $aa_sq[31:0] = $aa * $aa;
+         $bb_sq[31:0] = $bb * $bb;
+      @2
+         $cc_sq[31:0] = $aa_sq + $bb_sq;
+      @3
+         $cc[31:0] = sqrt($cc_sq);
+   
+   // Assert these to end simulation (before Makerchip cycle limit).
+   *passed = *cyc_cnt > 40;
+   *failed = 1'b0;
+\SV
+   endmodule
